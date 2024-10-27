65090be3d106beefb876e68a6855f5734449eb1d
Fabio <fabio_lutz@outlook.com>
2024-10-27 18:12:15 -0300

    Add explicit SQL type definition

--- src/main/java/org/project/userManagement/model/User.java
+++ src/main/java/org/project/userManagement/model/User.java
@@ -23,20 +23,20 @@ public class User {
     @Email(message = "Invalid email")
     @Size(min = 5, max = 50, message = "The email must be between 5 and 50 characters")
     @NotBlank(message = "The email must not be blank")
-    @Column(name = "email", unique = true, nullable = false, length = 50)
+    @Column(name = "email", columnDefinition = "VARCHAR(50)", unique = true, nullable = false, length = 50)
     private String email;
 
     @Size(min = 2, max = 100, message = "The username must be between 2 and 100 characters")
     @NotBlank(message = "The username must not be blank")
-    @Column(name = "username", unique = true, nullable = false, length = 100)
+    @Column(name = "username", columnDefinition = "VARCHAR(100)", unique = true, nullable = false, length = 100)
     private String username;
 
     @Size(min = 8, max = 128, message = "The password must be between 8 and 128 characters")
     @NotBlank(message = "The password must not be blank")
-    @Column(name = "password", nullable = false, length = 128)
+    @Column(name = "password", columnDefinition = "VARCHAR(128)", nullable = false, length = 128)
     private String password;
 
     @NotNull(message = "The role must not be null")
-    @Column(name = "role", nullable = false)
+    @Column(name = "role", columnDefinition = "SMALLINT", nullable = false)
     private UserRole role;
 }
