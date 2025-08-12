076e0ca08147077881480da267abe240e2fcb112
FabioLutz <135400867+FabioLutz@users.noreply.github.com>
2025-08-12 14:39:18 -0300

    refactor: remove bean validations from user entity

--- src/main/java/org/project/userManagement/model/User.java
+++ src/main/java/org/project/userManagement/model/User.java
@@ -1,10 +1,6 @@
 package org.project.userManagement.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,23 +18,15 @@ public class User {
     @Column(name = "id")
     private Long id;
 
-    @Email(message = "Invalid email")
-    @Size(min = 5, max = 50, message = "The email must be between 5 and 50 characters")
-    @NotBlank(message = "The email must not be blank")
     @Column(name = "email", columnDefinition = "VARCHAR(50)", unique = true, nullable = false, length = 50)
     private String email;
 
-    @Size(min = 2, max = 100, message = "The username must be between 2 and 100 characters")
-    @NotBlank(message = "The username must not be blank")
     @Column(name = "username", columnDefinition = "VARCHAR(100)", unique = true, nullable = false, length = 100)
     private String username;
 
-    @Size(min = 8, max = 128, message = "The password must be between 8 and 128 characters")
-    @NotBlank(message = "The password must not be blank")
     @Column(name = "password", columnDefinition = "VARCHAR(128)", nullable = false, length = 128)
     private String password;
 
-    @NotNull(message = "The role must not be null")
     @Column(name = "role", columnDefinition = "SMALLINT", nullable = false)
     private UserRole role;
 }
