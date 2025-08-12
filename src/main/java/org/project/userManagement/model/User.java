fc1fd819ace7689b2eb1bf7f0248a2a081cd9391
FabioLutz <135400867+FabioLutz@users.noreply.github.com>
2025-08-12 14:14:38 -0300

    refactor: remove unused hash and equals methods

--- src/main/java/org/project/userManagement/model/User.java
+++ src/main/java/org/project/userManagement/model/User.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
-@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "users_tb")
