ec52cb67d669a23eb2f5cc99d0f4e6c7a6f4c96b
FabioLutz <135400867+FabioLutz@users.noreply.github.com>
2025-10-05 01:08:02 -0300

    refactor: move register from auth to user

--- src/main/java/org/project/userManagement/controller/AuthController.java
+++ src/main/java/org/project/userManagement/controller/AuthController.java
@@ -3,32 +3,19 @@ package org.project.userManagement.controller;
 import jakarta.validation.Valid;
 import org.project.userManagement.dto.LoginResponseDto;
 import org.project.userManagement.dto.LoginUserDto;
-import org.project.userManagement.dto.RegisterUserDto;
-import org.project.userManagement.dto.UserDto;
 import org.project.userManagement.service.AuthService;
-import org.project.userManagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
 public class AuthController {
-    @Autowired
-    private UserService userService;
-
     @Autowired
     private AuthService authService;
 
-    @PostMapping("/register")
-    public ResponseEntity<UserDto> registerUser(@Valid @RequestBody RegisterUserDto registerUserDto) {
-        UserDto userDto = authService.registerUser(registerUserDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userDto);
-    }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> loginUser(@Valid @RequestBody LoginUserDto loginUserDto) {
