eea0cfcc1e7bf1123e2333ecd912a1acaa6eca67
FabioLutz <135400867+FabioLutz@users.noreply.github.com>
2025-10-05 01:08:02 -0300

    refactor(userMapper): remove unnecessary mapper instance

--- src/main/java/org/project/userManagement/mapper/UserMapper.java
+++ src/main/java/org/project/userManagement/mapper/UserMapper.java
@@ -1,15 +1,11 @@
 package org.project.userManagement.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 import org.project.userManagement.dto.RegisterUserDto;
 import org.project.userManagement.dto.UserDto;
 import org.project.userManagement.model.User;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
-
     UserDto userToUserDto(User user);
 
     User userDtoToUser(UserDto userDto);
