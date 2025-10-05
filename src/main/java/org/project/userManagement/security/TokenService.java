716d41f84bafc8c10a5631af8b67917a8dc73ed0
FabioLutz <135400867+FabioLutz@users.noreply.github.com>
2025-10-05 01:08:02 -0300

    fix(token): decrease token expiration time

--- src/main/java/org/project/userManagement/security/TokenService.java
+++ src/main/java/org/project/userManagement/security/TokenService.java
@@ -49,6 +49,6 @@ public class TokenService {
     }
 
     private Instant tokenExpirationDate() {
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusMinutes(15).toInstant(ZoneOffset.ofHours(-3));
     }
 }
